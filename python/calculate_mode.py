5fe06999710a39e133d0790ba32e044b2ada339a
robertuptc <robertuptc@gmail.com>
2025-03-11 11:55:29 -0400

    repo created

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,29 @@
-def calculate_mode():
-    pass
+def calculate_mode(list):
+    items_dict = create_dict(list)
+    answer = loop_dict(items_dict)
+    return answer
+
+def create_dict(l):
+    sorted_l = list(reversed(sorted(l)))
+    new_dict = {}
+
+    for element in sorted_l:
+        if element in new_dict:
+            new_dict[element] += 1
+        else:
+            new_dict[element] = 1
+    return new_dict
+
+def loop_dict(d):
+    count = 0
+    answer = []
+    for key, value in d.items():
+        if value > count:
+            count = value
+            answer = []
+            answer.append(key)
+        elif value == count:
+            count = value
+            answer.append(key)
+    print(answer)
+    return answer
--- python/calculate_mode_spec.py
+++ python/calculate_mode_spec.py
@@ -1,1 +1,8 @@
-# Write your own unit tests here!
+from calculate_mode import calculate_mode
+
+# calculate_mode([1,2,3,3])         # => [3]
+# calculate_mode([4.5, 0, 0])       # => [0]
+# calculate_mode([1.5, -1, 1, 1.5]) # => [1.5]
+# calculate_mode([1,1,2,2])         # => [1,2]
+# calculate_mode([1,2,3])           # => [1,2,3], because all occur with equal frequency
+calculate_mode(["who", "what", "where", "who"]) # => ["who"]
