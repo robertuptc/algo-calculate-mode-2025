b2ce3b795ce6e7d8f7d17e9e50a92ab0f9822592
robertuptc <robertuptc@gmail.com>
2025-03-11 11:26:42 -0400

    js problem finished

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,35 @@
-const calculateMode = () => {
-
+const calculateMode = (list) => {
+    if (list.length === 1) {
+        return list[0];
+    } else {
+        let objCount = countItems(list);
+        let mostFrequent = findFrequent(objCount);
+        console.log(mostFrequent)
+        return mostFrequent
+    }
 }
+
+function countItems(list) {
+    let obj = {};
+
+    for (let i = 0; i < list.length; i++) {
+        list[i] in obj ? obj[list[i]]++ : obj[list[i]] = 1;
+    }
+    return obj;
+}
+
+function findFrequent(obj) {
+    answer = [];
+    count = 0;
+    for (const[key, value] of Object.entries(obj)) {
+        if (value == count) {
+            answer.push(parseFloat(key))
+        } else if (value > count) {
+            answer = []
+            count = value
+            isNaN(parseFloat(key)) ? answer.push(key) : answer.push(parseFloat(key))
+        }
+    }
+    return answer
+}
+module.exports = {calculateMode}
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,8 @@
-// Write your own unit tests here!
+const cal = require("./calculateMode")
+
+// cal.calculateMode([1,2,3,3])         // => [3]
+// cal.calculateMode([4.5, 0, 0])       //  => [0]
+// cal.calculateMode([1.5, -1, 1, 1.5]) //  => [1.5]
+// cal.calculateMode([1,1,2,2])         //  => [1,2]
+// cal.calculateMode([1,2,3])           //  => [1,2,3], because all occur with equal frequency
+cal.calculateMode(["who", "what", "where", "who"]) //  => ["who"]
